577c5e5f93f1a713fbe5d1312a08f5f6cad80d3a
skudryavtsev <s2.kudryavtsev@itransition.com>
2018-12-10 14:05:12 +0300

    test gh-pages

---
+++ precache-manifest.1a55808dcf0b5020c68632c469cbeb9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08cbe5e2f91495199a07",
+    "url": "/news-react/static/js/runtime~main.08cbe5e2.js"
+  },
+  {
+    "revision": "b7e535ded2af544a81b7",
+    "url": "/news-react/static/js/main.b7e535de.chunk.js"
+  },
+  {
+    "revision": "0b52df4f42ccec3371a0",
+    "url": "/news-react/static/js/1.0b52df4f.chunk.js"
+  },
+  {
+    "revision": "b7e535ded2af544a81b7",
+    "url": "/news-react/static/css/main.bb6d76ea.chunk.css"
+  },
+  {
+    "revision": "52396be494f38d4087df54391c93e59d",
+    "url": "/news-react/index.html"
+  }
+];
